ad3f3b02a23e0bdbc77fe323010e2ba694001e50
Max Cohen <maxcohen@bu.edu>
2022-04-01 10:11:02 -0400

    Update sims

--- src/Utils/data_utils.jl
+++ src/Utils/data_utils.jl
@@ -6,3 +6,14 @@ Convert vector of vectors into matrix. If input is a single vector do nothing.
 """
 vec2mat(x::Vector{Vector{Float64}}) = reduce(hcat, x)
 vec2mat(x::Vector{Float64}) = x
+
+"""
+    vec2mat!(Σ::ControlAffineSystem)
+
+In-place modification of system trajectory
+"""
+function vec2mat!(Σ::ControlAffineSystem)
+    Σ.xs = vec2mat(Σ.xs)
+
+    return Σ
+end
