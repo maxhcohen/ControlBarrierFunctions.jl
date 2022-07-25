b0a693bc136ffbc6bb4ab3c8761902e8aff1705f
Max Cohen <maxcohen@bu.edu>
2022-07-24 20:20:53 -0400

    Fix quadratic cost

--- src/clf_quad_prog.jl
+++ src/clf_quad_prog.jl
@@ -26,47 +26,8 @@ Construct `CLFQuadProg`` from a `ControlAffineSystem`` and `ControlLyapunovFunct
 """
 function CLFQuadProg(Σ::ControlAffineSystem, CLF::ControlLyapunovFunction)
     # Set parameters for objective function
-    H = Σ.m == 1 ? 0.0 : zeros(Σ.m, Σ.m)
+    H = Σ.m == 1 ? 1.0 : Matrix(1.0I, Σ.m, Σ.m)
     F = Σ.m == 1 ? 0.0 : zeros(Σ.m)
-
-    # Construct quadratic program
-    function solve(x)
-        # Build QP and instantiate control decision variable
-        model = Model(OSQP.Optimizer)
-        set_silent(model)
-        Σ.m == 1 ? @variable(model, u) : @variable(model, u[1:Σ.m])
-
-        # Compute Lie derivatives
-        LfV = drift_lie_derivative(CLF, Σ, x)
-        LgV = control_lie_derivative(CLF, Σ, x)
-        γ = CLF.α(x)
-
-        # Check if we're relaxing the CLF constraint
-        if CLF.relax
-            @variable(model, δ)
-            @constraint(model, LfV + LgV*u <= -γ + δ)
-            @objective(model, Min, 0.5*u'*H*u + F'*u + CLF.p*δ^2)
-        else
-            @constraint(model, LfV + LgV*u <= -γ)
-            @objective(model, Min, 0.5*u'*H*u + F'*u)
-        end
-
-        # Add control bounds on system - recall these default to unbounded controls
-        if ~(Inf in Σ.b)
-            @constraint(model, Σ.A * u .<= Σ.b)
-        end
-
-        # Solve QP
-        optimize!(model)
-
-        return Σ.m == 1 ? value(u) : value.(u)
-    end
-
-    return CLFQuadProg(solve, H, F)
-end
-
-function CLFQuadProg(Σ::ControlAffineSystem, CLF::ControlLyapunovFunction, 
-                     H::Union{Float64, Matrix{Float64}}, F::Union{Float64, Vector{Float64}})
 
     # Construct quadratic program
     function solve(x)
