b4dc0aebc8b18fc8063def88c93d9acc9cabf727
maxcohen <maxcohen@caltech.edu>
2024-09-18 13:57:45 -0700

    Commenting

--- src/control_affine_system.jl
+++ src/control_affine_system.jl
@@ -1,5 +1,5 @@
 """
-    ControlAffineSystem
+	ControlAffineSystem
 
 Control affine system described by the dynamics ``ẋ = f(x) + g(x)u`` where ``x ∈ ℝⁿ`` is the system state, ``u ∈ ℝᵐ`` is the control input and ``f : ℝⁿ → ℝⁿ`` and ``g : ℝⁿ → ℝⁿˣᵐ`` represent the dynamics.
 
@@ -11,66 +11,72 @@ Control affine system described by the dynamics ``ẋ = f(x) + g(x)u`` where ``x
 - `g::Function` : control directions
 """
 struct ControlAffineSystem
-    name::String
-    n::Int
-    m::Int
-    f::Function
-    g::Function
+	name::String
+	n::Int
+	m::Int
+	f::Function
+	g::Function
 end
 
+##### ControlAffineSystem constructors #####
+
 """
-    ControlAffineSystem(n::Int, m::Int, f::Function, g::Function)
+	ControlAffineSystem(n::Int, m::Int, f::Function, g::Function)
 
 Construct a control affine system. Name defaults to `missing` if not provided.
 """
 ControlAffineSystem(n::Int, m::Int, f::Function, g::Function) =
-    ControlAffineSystem("", n, m, f, g)
+	ControlAffineSystem("", n, m, f, g)
+
+##### Functions to compute dynamics #####
 
 """
-    dynamics(Σ::ControlAffineSystem, x)
-    dynamics(Σ::ControlAffineSystem, x, u)
+	dynamics(Σ::ControlAffineSystem, x)
+	dynamics(Σ::ControlAffineSystem, x, u)
 
 Compute the dynamics `ẋ` of a control affine system
 """
 dynamics(Σ::ControlAffineSystem, x) = Σ.f(x)
 dynamics(Σ::ControlAffineSystem, x, u) = Σ.f(x) + Σ.g(x) * u
 
+##### Functions to run simulations #####
+
 """
-    simulate(Σ::ControlAffineSystem, x0, T::Float64)
+	simulate(Σ::ControlAffineSystem, x0, T::Float64)
 
 Simulate a control affine system from initial condition `x0` for `T` seconds.
 """
 function simulate(Σ::ControlAffineSystem, x0, T::Float64)
-    function odefun(dx, x, p, t)
-        return dx[1:(Σ.n)] .= dynamics(Σ, x)
-    end
+	function odefun(dx, x, p, t)
+		return dx[1:(Σ.n)] .= dynamics(Σ, x)
+	end
 
-    return solve(ODEProblem(odefun, x0, (0, T)))
+	return solve(ODEProblem(odefun, x0, (0, T)))
 end
 
 """
-    simulate(Σ::ControlAffineSystem, k::Function, x0, T::Float64)
+	simulate(Σ::ControlAffineSystem, k::Function, x0, T::Float64)
 
 Simulate a closed-loop control affine system from initial condition `x0` for `T` seconds. 
 """
 function simulate(Σ::ControlAffineSystem, k::Function, x0, T::Float64)
-    try
-        k(Σ.n == 1 ? rand() : rand(Σ.n), 0.0)
-    catch e
-        if isa(e, MethodError)
-            function odefun1(dx, x, p, t)
-                return dx[1:(Σ.n)] .= dynamics(Σ, x, k(x))
-            end
+	try
+		k(Σ.n == 1 ? rand() : rand(Σ.n), 0.0)
+	catch e
+		if isa(e, MethodError)
+			function odefun1(dx, x, p, t)
+				return dx[1:(Σ.n)] .= dynamics(Σ, x, k(x))
+			end
 
-            return solve(ODEProblem(odefun1, x0, (0, T)))
-        else
-            return e
-        end
-    else
-        function odefun2(dx, x, p, t)
-            return dx[1:(Σ.n)] .= dynamics(Σ, x, k(x, t))
-        end
+			return solve(ODEProblem(odefun1, x0, (0, T)))
+		else
+			return e
+		end
+	else
+		function odefun2(dx, x, p, t)
+			return dx[1:(Σ.n)] .= dynamics(Σ, x, k(x, t))
+		end
 
-        return solve(ODEProblem(odefun2, x0, (0, T)))
-    end
+		return solve(ODEProblem(odefun2, x0, (0, T)))
+	end
 end
